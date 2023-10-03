ff84ad294104d511ea7cd7097b7a688facb52c4c
Steven Tran <StevenDoTran@gmail.com>
2023-10-03 12:35:42 -0500

    V0.4: Added list of info with optional info

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
+  // Initialize as null when app starts so that the bottom portion of html file doesn't appear until a hero button is clicked
+  selectedHero?: Hero;
+
+  // Passed by object reference by heroes component html file
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
